b9308994e7bf0df55acd2cb291b033e3e9c6e358
aliyeff-f <eliyevnurlan811@gmail.com>
2024-04-08 00:07:40 +0400

    header

---
+++ Metalink/assets/js/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+
+/**
+ * NAVBAR TOGGLE FOR MOBILE
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+navToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+
+
+/**
+ * HEADER & BACK TOP BTN
+ * header and back top btn visible when window scroll down to 200px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElementOnScroll = function () {
+  if (window.scrollY > 200) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+window.addEventListener("scroll", activeElementOnScroll);
+
+
+
